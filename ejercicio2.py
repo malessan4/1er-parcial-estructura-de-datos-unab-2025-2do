874f15e9ed31d4466a74dd8a637908f61d0d5ec7
malessan4 <matialess@gmail.com>
2025-10-03 18:24:29 -0300

    ejercicio 2 completo

--- ejercicio2.py
+++ ejercicio2.py
@@ -8,6 +8,10 @@ class Propiedad:
         self.estado = estado
         self.premium = premium
 
+    def __str__(self):
+        premium_str = "Premium" if self.premium else "Standard"
+        return f"Código: {self.codigo}, Dirección: {self.direccion}, Tipo: {self.tipo}, Ambientes: {self.ambientes}, Precio: ${self.precio}, Estado: {self.estado}, {premium_str}"
+
 class Inmobiliaria:
     def __init__(self, nombre):
         self.nombre = nombre
@@ -18,19 +22,38 @@ class Inmobiliaria:
         print("Propiedad agregada exitosamente.")
 
     def buscar_por_tipo(self, tipo):
-        print(f"Propiedades del tipo: {self.tipo}")
-        for p in self.propiedaddes:
+        print(f"Propiedades del tipo: {tipo}")
+        for p in self.propiedades:
             if p.tipo.lower() == tipo.lower():
                 print(p)
 
+
     def buscar_por_precios(self, precio):
-        print(f"Propiedades de precio menor a: {self.precio}")
+        print(f"Propiedades de precio menor a: {precio}")
         for i in self.propiedades:
             if i.precio <= precio:
                 print(i)
+
+    
+    def promocion_premium(self):
+        print("Propiedades Premium:")
+        for j in self.propiedades:
+            if j.premium == True :
+                print(j)
 
 
 
 # Ejemplo de Uso
 
 inmobiliaria = Inmobiliaria("Inmobiliaria Central")
+p1 = Propiedad(1, "Calle 123", "Casa", 3, 150000, "Disponible", True)
+p2 = Propiedad(2, "Avenida 456", "Departamento", 2, 90000, "Vendido", False)   
+p3 = Propiedad(3, "Boulevard 789", "Casa", 4, 200000, "Disponible", True)
+
+inmobiliaria.agregar_propiedad(p1)
+inmobiliaria.agregar_propiedad(p2)
+inmobiliaria.agregar_propiedad(p3)
+
+inmobiliaria.buscar_por_tipo("Casa")    
+inmobiliaria.buscar_por_precios(160000)
+inmobiliaria.promocion_premium()
